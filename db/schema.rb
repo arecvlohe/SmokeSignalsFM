4884dc386aea5a3f43b88ed1319cf0679e61007e
Adam Recvlohe <adam.recvlohe@gmail.com>
2015-05-11 12:57:19 -0400

    Add counter_cache to Episodes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505190940) do
+ActiveRecord::Schema.define(version: 20150511162459) do
 
   create_table "episodes", force: :cascade do |t|
     t.string   "title"
